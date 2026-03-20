b67e8ddc88015e8bd5ebece5e33e15f4946b84e8
sacagnargem <sacagnargem@gmail.com>
2026-03-19 22:31:15 -0300

    Add files via upload

---
+++ brincadeira1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+char sexo;
+int idade;
+float peso,altura;
+char i;
+printf("digite seu sexo M(MASCULINO) ou F(FEMININO)\n");
+
+scanf("%c",&sexo);
+
+
+
+printf("digite sua idade\n");
+
+scanf("%d", &idade);
+
+printf("digite seu altura e peso COM UM PONTO (.) PARA SEPARAR OS DECIMAIS\n");
+
+scanf("%f%f", &altura,&peso);
+
+printf("seu sexo: %c sua idade: %d sua altura: %.2f seu peso: %.2f e seu mmc esta em: %.2f\n" ,sexo,idade,altura,peso, peso/(altura*altura));
+printf("e so isso\n");
+
+system("pause");
+return 0;
+}
